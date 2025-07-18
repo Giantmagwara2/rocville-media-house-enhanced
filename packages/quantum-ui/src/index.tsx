4cceecde92f5c7040c9c403a305b4855b9044e01
RocVille Media House <giantmagwara2@gmail.com>
2025-07-18 10:51:55 +0200

    Add TypeScript workspace config and package optimizations

---
+++ packages/quantum-ui/src/index.tsx
@@ -0,0 +1,5 @@
+export { default as AIConciergeChat } from './AIConciergeChat';
+export { default as PortfolioVisualization } from './PortfolioVisualization';
+
+// Re-export types
+export type { AIMessage, Portfolio } from '@rocville/types';
